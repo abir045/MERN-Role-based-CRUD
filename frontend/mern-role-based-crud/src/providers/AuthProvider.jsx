679da959a00c3a3acb574361318cb48a78e6ee0b
unknown <abir045@gmail.com>
2025-04-19 15:40:23 +0600

    updated frontend with firebase config and auth provider

---
+++ frontend/mern-role-based-crud/src/providers/AuthProvider.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { app } from "../Firebase/firebase.config";
+import AuthContext from "./AuthContext";
+
+// export const AuthContext = createContext(null);
+
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const googleSignIn = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = () => {
+    setLoading(true);
+    return signOut(auth);
+  };
+
+  const authInfo = {
+    user,
+    createUser,
+    signIn,
+    googleSignIn,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
---
+++ frontend/mern-role-based-crud/src/routes/Router.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Main from "../Layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+  },
+]);
